06cdba0bcb8364e875929c5d7f7dbbb54505c402
manuelmolinaf <manuelmolinaf@outlook.com>
2018-03-11 18:41:23 -0400

    defined some functions for matrix_4

--- src/BoilerPlate/matrix_4.cpp
+++ src/BoilerPlate/matrix_4.cpp
@@ -2,65 +2,128 @@
 
 namespace engine
 {
-	matrix_4::matrix_4()
+	namespace math
 	{
 
-		for (int row = 0; row < 4; row++)
+
+		matrix_4::matrix_4()
 		{
+
 			for (int column = 0; column < 4; column++)
 			{
-				if (row == column)
+				for (int row = 0; row < 4; row++)
 				{
-					matrix[row][column] = 1;
-				}
-				else
-				{
-					matrix[row][column] = 0;
+					if (column == row)
+					{
+						mMatrix[row][column] = 1;
+					}
+					else
+					{
+						mMatrix[row][column] = 0;
+					}
 				}
 			}
+
 		}
-	}
 
-	matrix_4::matrix_4(float first_position, float second_position, float third_position, float fourth_position, float fith_position, float sixth_position, float seventh_position,
-		               float eigth_position, float nineth_position, float tenth_position, float eleventh_position, float twelveth_position, float thirteenth_position, float fourteenth_position,
-		               float fithteenth_position, float sixteenth_position)
-	{
-
-		matrix[0][0] = first_position;
-		matrix[0][1] = second_position;
-		matrix[0][2] = third_position;
-		matrix[0][3] = fourth_position;
-		matrix[1][0] = fith_position;
-		matrix[1][1] = sixth_position;
-		matrix[1][2] = seventh_position;
-		matrix[1][3] = eigth_position;
-		matrix[2][0] = nineth_position;
-		matrix[2][1] = tenth_position;
-		matrix[2][2] = eleventh_position;
-		matrix[2][3] = twelveth_position;
-		matrix[3][0] = thirteenth_position;
-		matrix[3][1] = fourteenth_position;
-		matrix[3][2] = fithteenth_position;
-		matrix[3][3] = sixteenth_position;
-
-	}
-
-	matrix_4::matrix_4(float values_array[16])
-	{
-		
-		for (int array_position = 0; array_position < 16; array_position++)
+		matrix_4::matrix_4(float pFirstPosition, float pSecondPosition, float pThirdPosition, float pFourthPosition, float pFithPosition, float pSixthPosition, float pSeventhPosition,
+			float pEigthPosition, float pNinethPosition, float pTenthPosition, float pEleventhPosition, float pTwelvethPosition, float pThirteenthPosition, float pFourteenthPosition,
+			float pFithteenthPosition, float pSixteenthPosition)
 		{
-			for (int row = 0; row < 4; row++)
-			{
-				for (int column = 0; column < 4; column++)
-				{
-					matrix[row][column] = values_array[array_position];
-				}
-			}
+
+			mMatrix[0][0] = pFirstPosition;
+			mMatrix[1][0] = pSecondPosition;
+			mMatrix[2][0] = pThirdPosition;
+			mMatrix[3][0] = pFourthPosition;
+			mMatrix[0][1] = pFithPosition;
+			mMatrix[1][1] = pSixthPosition;
+			mMatrix[2][1] = pSeventhPosition;
+			mMatrix[3][1] = pEigthPosition;
+			mMatrix[0][2] = pNinethPosition;
+			mMatrix[1][2] = pTenthPosition;
+			mMatrix[2][2] = pEleventhPosition;
+			mMatrix[3][2] = pTwelvethPosition;
+			mMatrix[0][3] = pThirteenthPosition;
+			mMatrix[1][3] = pFourteenthPosition;
+			mMatrix[2][3] = pFithteenthPosition;
+			mMatrix[3][3] = pSixteenthPosition;
+
 		}
 
+		matrix_4::matrix_4(float pValuesArray[16])
+		{
+
+			for (int array_position = 0; array_position < 16; array_position++)
+			{
+				for (int row = 0; row < 4; row++)
+				{
+					for (int column = 0; column < 4; column++)
+					{
+						mMatrix[row][column] = pValuesArray[array_position];
+					}
+				}
+			}
+
+		}
+
+		float* matrix_4::get_row(int pRowNumber)
+		{
+			float row[4];
+			
+			for (int i = 0; i < 4; i++)
+			{
+				row[i] = mMatrix[pRowNumber][i];
+			}
+
+			return row;
+		}
+
+
+		float* matrix_4::get_column(int pColumnNumber)
+		{
+			float column[4];
+
+			for (int i = 0; i < 4; i++)
+			{
+				column[i] = mMatrix[i][pColumnNumber];
+			}
+
+			return column;
+		}
+
+		float* matrix_4::get_matrix()
+		{
+			return *mMatrix;
+		}
+
+
+		float* matrix_4::get_transpose()
+		{
+			float transposedMatrix[4][4];
+
+			for (int column = 0; column < 4; column++)
+			{
+				for (int row = 0; row < 4; row++)
+				{
+					transposedMatrix[row][column] = mMatrix[column][row];
+				}
+			}
+
+			return *transposedMatrix;
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
 	}
-
-
 
 }
--- src/BoilerPlate/matrix_4.hpp
+++ src/BoilerPlate/matrix_4.hpp
@@ -5,39 +5,47 @@
 
 namespace engine
 {
-	class matrix_4
+	namespace math
 	{
-	public:
+		class matrix_4
+		{
+		public:
 
-		//constructors
-		matrix_4();
+			//constructors
+			matrix_4();
 
-		matrix_4(float, float, float, float, float, float, float,
-			     float, float, float, float, float, float, float, 
-			     float, float);
+			matrix_4(float pFirstPosition, float pSecondPosition, float pThirdPosition, float pFourthPosition, float pFithPosition, float pSixthPosition, float pSeventhPosition,
+					 float pEigthPosition, float pNinethPosition, float pTenthPosition, float pEleventhPosition, float pTwelvethPosition, float pThirteenthPosition, float pFourteenthPosition,
+					 float pFithteenthPosition, float pSixteenthPosition);
 
-		matrix_4(float[16]);
+			matrix_4(float pValues[16]);
 
-		//public members
+			//public members
 
-		float matrix[4][4];
+			float mMatrix[4][4];
 
-		//public functions
+			//public functions
 
-		float* get_row();
-		float* get_column();
-		float* get_matrix();
-		float* get_transpose();
+			float* get_row(int pRowNumber);
+			float* get_column(int pColumnNumber);
+			float* get_matrix();
+			float* get_transpose();
 
-
-
-	private:
-
-
-
+			void set_identity();
+			
 
 
-	};
+
+
+		private:
+
+
+
+
+
+		};
+	}
+	
 }
 
 #endif
