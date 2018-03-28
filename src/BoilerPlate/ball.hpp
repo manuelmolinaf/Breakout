047e2cd6e288d9b8713a7350b10ed66efa819d67
manuelmolinaf <manuelmolinaf@outlook.com>
2018-03-28 16:38:37 -0400

    renamed some files

---
+++ src/BoilerPlate/ball.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#pragma once
+#ifndef _BALL_HPP_
+#define _BALL_HPP_
+
+#include"engine/core/game_object.hpp"
+
+namespace entities
+{
+	class ball : engine::core::game_object
+	{
+
+	public:
+		ball();
+		~ball();
+
+
+
+
+	private:
+
+
+
+	};
+
+}
+
+
+
+#endif
---
+++ src/BoilerPlate/block.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef _BLOCK_HPP_
+#define _BLOCK_HPP_
+
+#include"engine/core/game_object.hpp"
+
+namespace entities
+{
+	class block : engine::core::game_object
+	{
+
+	public:
+		block();
+		~block();
+
+		
+
+
+	private:
+
+		
+
+	};
+
+}
+
+
+
+#endif
